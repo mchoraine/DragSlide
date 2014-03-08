2a6108884d256ae8710bee80a58810e014b4a46f
chocho01 <martin.choraine@hotmail.fr>
2014-03-08 12:47:27 +0100

    Init git

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>DragSlide</title>
+	<link rel="stylesheet" href="./css/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+	<header>
+		<ul id="menu">
+			<li><h1>Catherine Goniot</h1></li>
+			<li>Serie1</li>
+			<li>Serie2</li>
+		</ul>
+	</header>
+	<section id="slider-big">
+
+	</section>
+	<section id="slide-bar">
+	
+	</section>
+	<section id="slider-miniature">
+
+	</section>
+</body>
+	<script src="./js/jquery.js"></script>
+	<script src="./js/jquery-ui.js"></script>
+	<script src="./js/main.js"></script>
+</html>
